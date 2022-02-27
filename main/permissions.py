ea87513c19160a7add7ea6e0b1006e2d980c5108
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-02-27 08:49:26 +0300

    add permissions for nurse and student clinician

--- main/permissions.py
+++ main/permissions.py
@@ -1,11 +1,24 @@
 from rest_framework import permissions
 
-from main.roles import DOCTOR, RECEPTIONIST
+from main.roles import DOCTOR, NURSE,\
+    RECEPTIONIST, STUDENT_CLINICIAN
+
 
 class IsReceptionist(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == RECEPTIONIST
 
+
 class IsDoctor(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == DOCTOR
+
+
+class IsNurse(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == NURSE
+
+
+class IsStudent_Clinician(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == STUDENT_CLINICIAN
