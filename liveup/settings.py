6cba285d737c8b546513079c2e07af37bee8c9a3
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-03-04 11:30:58 +0300

    install & setup corsheaders

--- liveup/settings.py
+++ liveup/settings.py
@@ -51,11 +51,13 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'dj_rest_auth',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
