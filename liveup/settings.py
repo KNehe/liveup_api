c1fa210770f275988d43c12698d037aaf2e9b184
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-03-16 18:37:34 +0300

    add proper db connection for heroku

--- liveup/settings.py
+++ liveup/settings.py
@@ -103,6 +103,9 @@ DATABASES = {
         'PORT': env('DB_PORT')
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
